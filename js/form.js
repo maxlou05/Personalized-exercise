aa4c3d969fad56791645f2720930e570c0e910aa
Zli123123 <zhenhaoli1313@gmail.com>
2022-02-20 12:16:15 -0500

    wyhat

--- js/form.js
+++ js/form.js
@@ -1,14 +1,14 @@
-function checkButton() {  
-    var getSelectedValue = document.querySelector( 
-        'input[name="intensity"]:checked'); 
-      
-    if(getSelectedValue != null) { 
-        document.getElementById("disp").innerHTML 
-            = getSelectedValue.value 
-            + " intensity is selected"; 
-    } 
-    else { 
-        document.getElementById("error").innerHTML 
-            = "*You have not selected any season"; 
-    } 
-}  
+function checkButton() {    
+    var getSelectedValue = document.querySelector(   
+        'input[name="season"]:checked');   
+        
+    if(getSelectedValue != null) {   
+        document.getElementById("disp").innerHTML   
+            = getSelectedValue.value   
+            + " season is selected";   
+    }   
+    else {   
+        document.getElementById("error").innerHTML   
+            = "*You have not selected any season";   
+    }   
+} 
